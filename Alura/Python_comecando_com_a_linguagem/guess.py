040ed5ecbcc1e2d5484b44c946e494a1a47a33e4
DornelesArthur <arthhdorneles@gmail.com>
2024-03-20 15:19:19 -0300

    feat: add number of chances

--- Alura/Python_comecando_com_a_linguagem/guess.py
+++ Alura/Python_comecando_com_a_linguagem/guess.py
@@ -3,28 +3,25 @@ print("Welcome to guess the number!")
 print("****************************")
 
 secret_number = 42
+number_chances = 4
 
 try:
-    guess = int(input("Guess the number: "))
+    while (number_chances):
+        guess = int(input(f"Guess the number ({number_chances} chances left): "))
 
-    print("Your guess is: ", guess)
+        print("Your guess is: ", guess)
 
-    correct = secret_number == guess
-    higher = secret_number > guess
-    if(correct):
-        print("You have guess correctly")
-    else:
-        if (higher):
-            print("The secret number is higher than ", guess)
-        else:
-            print("The secret number is lower than ", guess)
-
-        guess = int(input("You have a second chance. Guess again: "))
         correct = secret_number == guess
+        higher = secret_number > guess
         if(correct):
             print("You have guess correctly")
+            number_chances = 0
         else:
-            print("You have guess wrong")
-
+            if (higher):
+                print("The secret number is higher than ", guess)
+            else:
+                print("The secret number is lower than ", guess)
+            number_chances-=1
+    print("Game has ended")
 except ValueError:
     print("Only numbers are accepted!")
